097ffb0c258da1fa1f44e760c236369daa71912f
PrinceYauj <c2h5ohmel@yandex.ru>
2021-09-09 19:43:49 +0300

    Remove incorrect controller restrictions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,13 @@
 class BlogsController < InheritedResources::Base
-  before_action :require_login
+  #before_action :require_login
   private
 
   def blog_params
     params.require(:blog).permit(:title, :body, :user_id)
   end
 
-  def require_login
-    flash[:error] = "you must be logged in to see this page"
-    redirect_to root_path
-  end
+  #  def require_login
+  #    flash[:error] = "you must be logged in to see this page"
+  #    redirect_to root_path
+  # end
 end
