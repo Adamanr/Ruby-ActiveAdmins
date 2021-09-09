8ddfdb05db4d37d4bd566a56614f56d0b3f295e3
PrinceYauj <c2h5ohmel@yandex.ru>
2021-09-09 17:40:23 +0300

    Add before action hook

--- app/admin/blogs.rb
+++ app/admin/blogs.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Blog do
   permit_params :title, :body, :user_id
+
+  controller do
+    before_action :require_login
+    def require_login
+      flash[:error] = 'you must be logged in to see this page'
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,13 @@
 class RepliesController < InheritedResources::Base
-
+  before_action :require_login
   private
 
     def reply_params
       params.require(:reply).permit(:text, :user_id, :blog_id)
     end
 
+  def require_login
+    flash[:error] = 'you must be logged in to see this page'
+    redirect_to root_path
+  end
 end
